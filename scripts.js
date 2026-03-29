dbe19ef494f39c6717af2e5a37159d7a890ff7a6
tbailey32 <tbailey32@mail.valenciacollege.edu>
2026-03-28 22:13:23 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function videoFunction(){
+            document.getElementById("myComment").innerHTML += "A great vacation video";
+}
+
+function emailMessage(){
+    document.getElementById("emailBox").innerHTML += " Please enter a valid email address";    
+}
+
+function blankMessage(){
+    document.getElementById("emailBox").innerHTML = "Email:";
+}
